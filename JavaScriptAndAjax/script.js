518d84542f31975e2b0271b2647df96c52524df9
Brett Morin <brettmorin2@gmail.com>
2016-04-13 12:16:10 -0700

    Added code for Ajax examples with Jquery

--- JavaScriptAndAjax/script.js
+++ JavaScriptAndAjax/script.js
@@ -1,40 +1,48 @@
-var mybutton = document.getElementById('loadbutton');
-mybutton.onclick = function() {
-    var request;
-    if(window.XMLHttpRequest) {
-        request = new XMLHttpRequest();
-    } else {
-        request = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-
-//request.open('GET', 'data.xml');
-//request.onreadystatechange = function() {
-//    if((request.readyState ===4) && (request.status ===200)) {
-//        var items = request.responseXML.getElementsByTagName('name');
-//        var output = '<ul>';
-//        for(var i = 0; i < items.length; i++) {
-//            output += '<li>' + items[i].firstChild.nodeValue + '</li>';
-//        }
-//        output += '</ul>';
-//        document.getElementById('update').innerHTML = output;
-//
+$.getJSON('data.json', function(data) {
+    var output = '<ul>';
+    $.each(data, function(key, val) {
+       output += '<li>' + val.name + '</li>';
+    });
+        output += '</ul>';
+    $('#update').append(output);
+});
+//var mybutton = document.getElementById('loadbutton');
+//mybutton.onclick = function() {
+//    var request;
+//    if(window.XMLHttpRequest) {
+//        request = new XMLHttpRequest();
+//    } else {
+//        request = new ActiveXObject("Microsoft.XMLHTTP");
 //    }
-//}
-
-    request.open('GET', 'data.json');
-    request.onreadystatechange = function() {
-        if((request.readyState ===4) && (request.status ===200)) {
-            var items = JSON.parse(request.responseText);
-
-            var output = '<ul>';
-            for(var key in items) {
-                output += '<li>' + items[key].name + '</li>';
-            }
-            output += '</ul>';
-            document.getElementById('update').innerHTML = output;
-        }
-    }
-
-    request.send();
-} // load AJAX
+//
+////request.open('GET', 'data.xml');
+////request.onreadystatechange = function() {
+////    if((request.readyState ===4) && (request.status ===200)) {
+////        var items = request.responseXML.getElementsByTagName('name');
+////        var output = '<ul>';
+////        for(var i = 0; i < items.length; i++) {
+////            output += '<li>' + items[i].firstChild.nodeValue + '</li>';
+////        }
+////        output += '</ul>';
+////        document.getElementById('update').innerHTML = output;
+////
+////    }
+////}
+//
+//    request.open('GET', 'data.json');
+//    request.onreadystatechange = function() {
+//        if((request.readyState ===4) && (request.status ===200)) {
+//            var items = JSON.parse(request.responseText);
+//
+//            var output = '<ul>';
+//            for(var key in items) {
+//                output += '<li>' + items[key].name + '</li>';
+//            }
+//            output += '</ul>';
+//            document.getElementById('update').innerHTML = output;
+//        }
+//    }
+//
+//    request.send();
+//} // load AJAX
 
