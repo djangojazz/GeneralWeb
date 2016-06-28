2cab25d42a847c1a7ee13af3ff506bac70a32cf9
Brett Morin <brettmorin2@gmail.com>
2016-06-28 12:36:41 -0700

    Added another route

--- rouxmeet/routes/index.js
+++ rouxmeet/routes/index.js
@@ -5,12 +5,48 @@ var appdata = require('../data.json');
 /* GET home page. */
 router.get('/', function(req, res) {
   var myArtwork = [];
+  var myArtists = [];
+
+  myArtists = appdata.speakers;
   appdata.speakers.forEach(function(item) {
     myArtwork = myArtwork.concat(item.artwork);
   });
   res.render('index', {
     title: 'Home',
-    artwork: myArtwork
+    artwork: myArtwork,
+    artists: myArtists
+  });
+});
+
+router.get('/speakers', function(req, res) {
+  var myArtwork = [];
+  var myArtists = [];
+
+  myArtists = appdata.speakers;
+  appdata.speakers.forEach(function(item) {
+    myArtwork = myArtwork.concat(item.artwork);
+  });
+  res.render('speakers', {
+    title: 'Speakers',
+    artwork: myArtwork,
+    artists: myArtists
+  });
+});
+
+router.get('/speakers/:speakerid', function(req, res) {
+  var myArtwork = [];
+  var myArtists = [];
+
+  appdata.speakers.forEach(function(item) {
+    if(item.shortname == req.params.speakerid) {
+      myArtists.push(item);
+      myArtwork = myArtwork.concat(item.artwork);
+    }
+  });
+  res.render('speakers', {
+    title: 'Speakers',
+    artwork: myArtwork,
+    artists: myArtists
   });
 });
 
