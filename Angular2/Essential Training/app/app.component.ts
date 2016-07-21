f590baa28afdb12001c868d9f19fb66b5db16814
Brett Morin <brettmorin2@gmail.com>
2016-07-21 12:32:47 -0700

    Made changes to paly with property binding

--- Angular2/Essential Training/app/app.component.ts
+++ Angular2/Essential Training/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from 'angular2/core';
+import {MediaItemComponent} from './media-item.component'
 
 @Component({
     selector: 'app',
-    template: '<h1>My App</h1>'
+    directives: [MediaItemComponent],
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']
 })
 
-export class AppComponent {
-
-}
+export class AppComponent {}
