bc800da70d0b29671a8abff6280c2c0131c6f3a2
Brett Morin <brettmorin2@gmail.com>
2016-07-28 13:37:00 -0700

    Made changes for routing

--- Angular2/Essential Training/app/media-item.component.ts
+++ Angular2/Essential Training/app/media-item.component.ts
@@ -6,7 +6,7 @@ import {Component, Input, Output, EventEmitter} from 'angular2/core';
     styleUrls: ['app/media-item.component.css']
 })
 export class MediaItemComponent {
-    @Input('mediaItem') Item;
+    @Input('mediaItem') mediaItem;
     @Output('deleted') delete = new EventEmitter();
 
     onDelete() {
