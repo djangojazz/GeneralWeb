6e032d805a11bcbf8e176de78ce5af87dabfc2be
Brett Morin <brettmorin2@gmail.com>
2016-07-29 13:27:16 -0700

    Updates with Forms

--- Angular2/Essential Training/app/app.component.ts
+++ Angular2/Essential Training/app/app.component.ts
@@ -1,27 +1,31 @@
 import {Component} from 'angular2/core';
 import {MediaItemComponent} from './media-item.component'
 import {MediaItemListComponent} from './media-item-list.component';
+import {MediaItemFormComponent} from './media-item-form.component';
 
 @Component({
     selector: 'app',
-    directives: [MediaItemComponent, MediaItemListComponent],
+    directives: [MediaItemComponent, MediaItemListComponent, MediaItemFormComponent],
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css']
 })
 
 export class AppComponent {
-    onMediaItemDeleted(mediaItem) {
-        console.log(mediaItem);
-        
-    }
 
-    firstMediaItem = {
-        id: 1,
-        name: "Firebug",
-        medium: "Series",
-        category: "Science Fiction",
-        year: 2010,
-        watchedOn: null,
-        isFavorite: false,
-    }
+    //Synthesizing a delete
+    // onMediaItemDeleted(mediaItem) {
+    //     console.log(mediaItem);
+        
+    // }
+
+//This was for the media-item.component done manually in the first section
+    // firstMediaItem = {
+    //     id: 1,
+    //     name: "Firebug",
+    //     medium: "Series",
+    //     category: "Science Fiction",
+    //     year: 2010,
+    //     watchedOn: null,
+    //     isFavorite: false,
+    // }
 }
