2b63d8be554cb0a9a5b27d0ad8cdd8c9bac8e949
djangojazz <brettmorin2@gmail.com>
2016-07-31 09:30:26 -0700

    added opaque token elements

--- Angular2/Essential Training/app/main.ts
+++ Angular2/Essential Training/app/main.ts
@@ -2,12 +2,10 @@ import {bootstrap} from 'angular2/platform/browser'
 import {AppComponent} from './app.component';
 import {MediaItemService} from './media-item.service';
 import {provide} from 'angular2/core'
+import {LOOKUP_LISTS, lookupLists} from './providers';
 
-var lookupLists = {
-    mediums: ['Movies', 'Series']
-};
 
 bootstrap(AppComponent, [
     MediaItemService,
-    provide('LOOKUP_LISTS', { useValue: lookupLists})
+    provide(LOOKUP_LISTS, { useValue: lookupLists})
 ]);
