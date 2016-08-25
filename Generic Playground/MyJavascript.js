39ba3bd0814b2167a393ee7bcaa27ebc136fc478
Brett Morin <brettmorin2@gmail.com>
2016-08-25 13:05:56 -0700

    made changes to javascript

--- Generic Playground/MyJavascript.js
+++ Generic Playground/MyJavascript.js
@@ -2,8 +2,7 @@ var stepNumber = 1;
 var thing = "ants";
 
 function DoStuff() {
-    console.time("Steps time");
-
+    console.time("Steps time")
     console.group("Steps");
     FirstStep();
     stepNumber++;
@@ -17,6 +16,73 @@ function DoStuff() {
     LoadBanners();
     SoundTrumpets();
     console.groupEnd();
+
+    document.getElementById("chromeImage").className = "smallImage";
+}
+
+function LoadResourcesData() {
+    //CreateWebSQLData();
+    CreateIndexedDbData();
+    CreateLocalAndSessionStorageData();
+    CreateCookieData();
+}
+
+// Looks like this is no longer used
+// function CreateWebSQLData() {
+//     var db = openDatabase('testdb', '1.0', 'test database', 2 * 1024 * 1024);
+//     db.transaction(function (tx) {
+//             tx.executeSQL('CREATE TABLE test (id unique, data)');
+//             tx.executeSQL('INSERT INTO test (id, data) VALUES (1, "test")' );
+//     });
+// }
+
+function CreateIndexedDbData() {
+    console.group("IndexDB");
+
+    var db;
+    var request = indexedDB.open("IndexDB Test DB", 7);
+    request.onerror = function(event) {
+        console.log("Couldn't use IndexDB");
+    };
+    request.onsuccess = function(event) {
+        db = request.result;
+        console.log("Opened database!");
+    }
+
+    var customerData = [
+        { id: 1, name: "Joe"},
+        { id: 2, name: "Bob"}
+    ];
+
+    request.onerror = function(event) {
+    };
+
+    request.onupgradeneeded = function(event) {
+        console.log("Upgrading database");
+        var db = event.target.result;
+
+        if(db.objectStoreNames.contains("customers")) {
+            db.deleteObjectStore("customers");
+        }
+
+        var objectStore = db.createObjectStore("customers", { keyPath: "id"});
+        objectStore.createIndex("name", "name", { unique: false});
+
+        for (var i in customerData) {
+            console.log("Adding customer: %s", i.name);
+            objectStore.add(customerData[i]);
+        }
+    };
+    console.groupEnd();
+}
+
+function CreateLocalAndSessionStorageData() {
+    localStorage.setItem("test key", "test value");
+    sessionStorage.setItem("test session key", "test session value");
+}
+
+function CreateCookieData() {
+    document.cookie - "cookiekey = cookievalue";
 }
 
 function FirstStep() {
