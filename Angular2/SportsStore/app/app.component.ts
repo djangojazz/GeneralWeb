29b338b3b8380474125fa947ec9fdae4c4d56ff7
Brett Morin <brettmorin2@gmail.com>
2017-07-20 13:00:52 -0700

    Added files for data stores and models

---
+++ Angular2/SportsStore/app/app.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "app",
+    template: `<div class="bg-success p-a-1 text-xs-center">
+                        This is Brett's SportsStore
+                </div>`
+})
+export class AppComponent { }
