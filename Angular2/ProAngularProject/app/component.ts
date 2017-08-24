d894ab2c3d83b14b0dba3d8be6d9ce23124a9441
Brett Morin <brettmorin2@gmail.com>
2017-08-24 11:57:54 -0700

    End of Chapter 12

--- Angular2/ProAngularProject/app/component.ts
+++ Angular2/ProAngularProject/app/component.ts
@@ -1,5 +1,6 @@
-﻿import { Component } from "@angular/core";
+﻿import { ApplicationRef, Component } from "@angular/core";
 import { Model } from "./repository.model";
+import { Product } from "./product.model";
 
 @Component({
     selector: "app",
@@ -7,4 +8,40 @@ import { Model } from "./repository.model";
 })
 export class ProductComponent {
     model: Model = new Model();
+
+    constructor(ref: ApplicationRef) {
+        (<any>window).appRef = ref;
+        (<any>window).model = this.model;
+    }
+
+    getProductByPosition(position: number): Product{
+        return this.model.getProducts()[position];
+    }
+
+    getClassesByPosition(position: number): string{
+        let product = this.getProductByPosition(position);
+        return "p-a-1 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    }
+
+    // getClasses(key: number): string {
+    //     let product = this.model.getProduct(key);
+    //     return "p-a-1 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    // }
+
+    // getClassMap(key: number): Object{
+    //     let product = this.model.getProduct(key);
+    //     return {
+    //         "text-xs-center bg-danger": product.name == "Kayak",
+    //         "bg-info": product.price < 50
+    //     }
+    // }
+
+    // getStyles(key: number) {
+    //     let product = this.model.getProduct(key);
+    //     return {
+    //         fontSize: "30px",
+    //         "margin.px": 100,
+    //         color: product.price > 50 ? "red" : "green"
+    //     }
+    // }
 }
