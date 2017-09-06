e0120c186143de717f2c82adef7419208f5984a1
Brett Morin <brettmorin2@gmail.com>
2017-09-06 12:29:45 -0700

    changes to module

--- Angular2/ProAngularProject/app/attr.directive.js
+++ Angular2/ProAngularProject/app/attr.directive.js
@@ -8,22 +8,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 var core_1 = require("@angular/core");
-var platform_browser_1 = require("@angular/platform-browser");
-var component_1 = require("./component");
-var forms_1 = require("@angular/forms");
-var attr_directive_1 = require("./attr.directive");
-var AppModule = (function () {
-    function AppModule() {
+var PaAttrDirective = (function () {
+    function PaAttrDirective(element, bgClass) {
+        element.nativeElement.classList.add(bgClass || "bg-success");
     }
-    AppModule = __decorate([
-        core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule, forms_1.FormsModule, forms_1.ReactiveFormsModule],
-            declarations: [component_1.ProductComponent, attr_directive_1.PaAttrDirective],
-            bootstrap: [component_1.ProductComponent]
-        }), 
-        __metadata('design:paramtypes', [])
-    ], AppModule);
-    return AppModule;
+    PaAttrDirective = __decorate([
+        core_1.Directive({
+            selector: "[pa-attr]",
+        }),
+        __param(1, core_1.Attribute("pa-attr-class")), 
+        __metadata('design:paramtypes', [core_1.ElementRef, String])
+    ], PaAttrDirective);
+    return PaAttrDirective;
 }());
-exports.AppModule = AppModule;
+exports.PaAttrDirective = PaAttrDirective;
