6abeb1f56b244ddd5c58fe5edfb687673b71ef80
Brett Morin <brettmorin2@gmail.com>
2017-10-30 12:50:26 -0700

    Templasized both sides

--- Angular2/ProAngularProject/app/productForm.component.js
+++ Angular2/ProAngularProject/app/productForm.component.js
@@ -9,13 +9,32 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var product_model_1 = require("./product.model");
+var form_model_1 = require("./form.model");
 var ProductFormComponent = (function () {
     function ProductFormComponent() {
+        this.form = new form_model_1.ProductFormGroup();
+        this.newProduct = new product_model_1.Product();
+        this.formSubmitted = false;
+        this.newProductEvent = new core_1.EventEmitter();
     }
+    ProductFormComponent.prototype.submitForm = function (form) {
+        this.formSubmitted = true;
+        if (form.valid) {
+            this.newProductEvent.emit(this.newProduct);
+            this.newProduct = new product_model_1.Product();
+            this.form.reset();
+            this.formSubmitted = false;
+        }
+    };
+    __decorate([
+        core_1.Output("paNewProduct"), 
+        __metadata('design:type', Object)
+    ], ProductFormComponent.prototype, "newProductEvent", void 0);
     ProductFormComponent = __decorate([
         core_1.Component({
             selector: "paProductForm",
-            template: "<div>This is the form component</div>"
+            templateUrl: "app/productForm.component.html"
         }), 
         __metadata('design:paramtypes', [])
     ], ProductFormComponent);
