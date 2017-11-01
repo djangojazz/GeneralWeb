bafba5f5e0a3d27b9ff090a87025da537a505ab8
Brett Morin <brettmorin2@gmail.com>
2017-11-01 12:52:41 -0700

    Created custom pipe

--- Angular2/ProAngularProject/app/app.module.js
+++ Angular2/ProAngularProject/app/app.module.js
@@ -21,6 +21,7 @@ var cellColorSwitcher_directive_1 = require("./cellColorSwitcher.directive");
 var productTable_component_1 = require("./productTable.component");
 var productForm_component_1 = require("./productForm.component");
 var toggleView_component_1 = require("./toggleView.component");
+var addTax_pipe_1 = require("./addTax.pipe");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -30,7 +31,7 @@ var AppModule = (function () {
             declarations: [component_1.ProductComponent, attr_directive_1.PaAttrDirective, twoway_directive_1.PaModel,
                 paif_directive_1.PaStructureDirective, iterator_directive_1.PaIteratorDirective,
                 cellColor_directive_1.PaCellColor, cellColorSwitcher_directive_1.PaCellColorSwitcher, productTable_component_1.ProductTableComponent,
-                productForm_component_1.ProductFormComponent, toggleView_component_1.PaToggleView],
+                productForm_component_1.ProductFormComponent, toggleView_component_1.PaToggleView, addTax_pipe_1.PaAddTaxPipe],
             bootstrap: [component_1.ProductComponent]
         }), 
         __metadata('design:paramtypes', [])
