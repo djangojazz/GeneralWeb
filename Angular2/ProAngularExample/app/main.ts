b08540a9cccc8e7edb45c7691acd44eeb8e2dcc2
Brett Morin <brettmorin2@gmail.com>
2017-12-04 15:04:31 -0800

    Removed excess

---
+++ Angular2/ProAngularExample/app/main.ts
@@ -0,0 +1,4 @@
+import { AppModule } from '../../ProAngularProject/app/app.module';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
