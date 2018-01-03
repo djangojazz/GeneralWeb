d7c4b5699a1d027444f97894bcf1cb73c63256e7
Brett Morin <brettmorin2@gmail.com>
2018-01-03 14:03:27 -0800

    Added loading splash screen with routing

--- Angular2/ProAngularExample/app/app.routing.js
+++ Angular2/ProAngularExample/app/app.routing.js
@@ -5,15 +5,18 @@ var form_component_1 = require("./core/form.component");
 var notFound_component_1 = require("./core/notFound.component");
 var productCount_component_1 = require("./core/productCount.component");
 var categoryCount_component_1 = require("./core/categoryCount.component");
+var model_resolver_1 = require("./model/model.resolver");
 var childRoutes = [
-    { path: "products", component: productCount_component_1.ProductCountComponent },
-    { path: "categories", component: categoryCount_component_1.CategoryCountComponent },
-    { path: "", component: productCount_component_1.ProductCountComponent }
+    { path: "",
+        children: [{ path: "products", component: productCount_component_1.ProductCountComponent },
+            { path: "categories", component: categoryCount_component_1.CategoryCountComponent },
+            { path: "", component: productCount_component_1.ProductCountComponent }],
+        resolve: { model: model_resolver_1.ModelResolver }
+    }
 ];
 var routes = [
     { path: "form/:mode/:id", component: form_component_1.FormComponent },
     { path: "form/:mode", component: form_component_1.FormComponent },
-    { path: "does", redirectTo: "/form/create", pathMatch: "prefix" },
     { path: "table", component: table_component_1.TableComponent, children: childRoutes },
     { path: "table/:category", component: table_component_1.TableComponent, children: childRoutes },
     { path: "", redirectTo: "/table", pathMatch: "full" },
